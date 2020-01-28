07d6c518536ee66507e042a2541f0a3511e85ad9
Frederic Hemberger <mail@frederic-hemberger.de>
2020-01-28 16:11:52 +0100

    Remove IIFE

--- extension/js/background.js
+++ extension/js/background.js
@@ -1,74 +1,70 @@
 /* global chrome, console */
 
-(function () {
-  'use strict'
+const defaultPaths = [
+  '^/metrics',
+  '^/federate',
+  '^/probe',
+  '^/prometheus',
+  '^/actuator/prometheus'
+]
 
-  const defaultPaths = [
-    '^/metrics',
-    '^/federate',
-    '^/probe',
-    '^/prometheus',
-    '^/actuator/prometheus'
-  ]
+// Listen for requests from content pages wanting to set up a port
+chrome.runtime.onConnect.addListener(port => {
+  if (port.name !== 'promformat') {
+    console.error(`[Prometheus Formatter] unknown port name "${port.name}". Aborting.`)
+    return
+  }
 
-  // Listen for requests from content pages wanting to set up a port
-  chrome.runtime.onConnect.addListener(port => {
-    if (port.name !== 'promformat') {
-      console.error(`[Prometheus Formatter] unknown port name "${port.name}". Aborting.`)
+  port.onMessage.addListener(msg => {
+    if (msg.name !== 'SENDING TEXT') {
       return
     }
 
-    port.onMessage.addListener(msg => {
-      if (msg.name !== 'SENDING TEXT') {
-        return
-      }
+    const html = msg.payload
+      .split(/\r?\n/)
+      .map(line => {
+        // line is a comment
+        if (/^#/.test(line)) {
+          return `<span class="comment">${line}</span>`
+        }
 
-      const html = msg.payload
-        .split(/\r?\n/)
-        .map(line => {
-          // line is a comment
-          if (/^#/.test(line)) {
-            return `<span class="comment">${line}</span>`
+        // line is a metric
+        // Named RegExp groups not supported by Firefox:
+        // https://bugzilla.mozilla.org/show_bug.cgi?id=1362154
+        // const tmp = line.match(/^(?<metric>[\w_]+)(?:\{(?<tags>.*)\})?\x20(?<value>.+)/)
+        const tmp = line.match(/^([\w_]+)(?:\{(.*)\})?\x20(.+)/)
+
+        if (tmp && tmp.length > 1) {
+          let [ _, metric, tags, value ] = tmp
+          if (tags) {
+            tags = tags.replace(/([^,]+?)="(.*?)"/g, '<span class="label-key">$1</span>="<span class="label-value">$2</span>"')
+            tags = `{${tags}}`
           }
 
-          // line is a metric
-          // Named RegExp groups not supported by Firefox:
-          // https://bugzilla.mozilla.org/show_bug.cgi?id=1362154
-          // const tmp = line.match(/^(?<metric>[\w_]+)(?:\{(?<tags>.*)\})?\x20(?<value>.+)/)
-          const tmp = line.match(/^([\w_]+)(?:\{(.*)\})?\x20(.+)/)
+          return `<span class="metric">${metric}</span>${tags || ''} <span class="value">${value}</span>`
+        }
 
-          if (tmp && tmp.length > 1) {
-            let [ _, metric, tags, value ] = tmp
-            if (tags) {
-              tags = tags.replace(/([^,]+?)="(.*?)"/g, '<span class="label-key">$1</span>="<span class="label-value">$2</span>"')
-              tags = `{${tags}}`
-            }
-
-            return `<span class="metric">${metric}</span>${tags || ''} <span class="value">${value}</span>`
-          }
-
-          // line is something else, do nothing
-          return line
-        })
-        .join('<br>')
-
-      // Post the HTML string to the content script
-      port.postMessage({
-        name: 'FORMATTED',
-        payload: html
+        // line is something else, do nothing
+        return line
       })
+      .join('<br>')
 
-      // Disconnect
-      port.disconnect()
+    // Post the HTML string to the content script
+    port.postMessage({
+      name: 'FORMATTED',
+      payload: html
     })
-  })
 
-  // Set default paths on extension installation and update
-  chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.get({ paths: [] }, storedData => {
-      if (!storedData.paths.length) {
-        chrome.storage.sync.set({ paths: defaultPaths })
-      }
-    })
+    // Disconnect
+    port.disconnect()
   })
-}())
+})
+
+// Set default paths on extension installation and update
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.get({ paths: [] }, storedData => {
+    if (!storedData.paths.length) {
+      chrome.storage.sync.set({ paths: defaultPaths })
+    }
+  })
+})
--- extension/js/content.js
+++ extension/js/content.js
@@ -1,98 +1,94 @@
 /* global chrome */
 
-(function () {
-  'use strict'
+const compress = (text) => text.replace(/\s+/g, '')
 
-  const compress = (text) => text.replace(/\s+/g, '')
+const maxBodyLength = 3000000 // 3MB
 
-  const maxBodyLength = 3000000 // 3MB
-
-  const style = compress(`
-    pre {
-      display:none
-    }
-    #promformat {
-      font-family: monospace;
-      word-wrap: break-word;
-      white-space: pre-wrap;
-    }
-    .comment {
-      color: #6a737d;
-      display: inline-block;
-    }
-    br + .comment {
-      padding-top: 1em;
-    }
-    .comment + br + .comment {
-        padding-top: 0;
-    }
-
-    .metric      { color: #000 }
-    .value       { color: #ff20ed }
-    .label-key   { color: blue }
-    .label-value { color: green }
-    `)
-
-  const port = chrome.runtime.connect({ name: 'promformat' })
-
-  // Add listener to receive response from BG when ready
-  port.onMessage.addListener(msg => {
-    switch (msg.name) {
-      case 'FORMATTED' :
-        // Insert CSS
-        const promformatStyle = document.createElement('style')
-        document.head.appendChild(promformatStyle)
-        promformatStyle.insertAdjacentHTML('beforeend', style)
-
-        // Insert HTML content
-        const promformatContent = document.createElement('div')
-        promformatContent.id = 'promformat'
-        document.body.appendChild(promformatContent)
-
-        promformatContent.innerHTML = msg.payload
-        break
-
-      default :
-        throw new Error(`Message not understood: ${msg.name}`)
-    }
-  })
-
-  function ready (data) {
-    // Check if it is a Prometheus plain text response
-    // This is quite a basic assumption, as the browser cannot access the
-    // 'version' part of the content type to verify.
-    const paths = data.paths.length ? data.paths : []
-
-    if (document.contentType !== 'text/plain') {
-      return
-    }
-
-    for (var i = 0; i < paths.length; ++i) {
-      if (document.location.pathname.match(paths[i])) {
-        format()
-        break
-      }
-    }
+const style = compress(`
+  pre {
+    display:none
+  }
+  #promformat {
+    font-family: monospace;
+    word-wrap: break-word;
+    white-space: pre-wrap;
+  }
+  .comment {
+    color: #6a737d;
+    display: inline-block;
+  }
+  br + .comment {
+    padding-top: 1em;
+  }
+  .comment + br + .comment {
+      padding-top: 0;
   }
 
-  function format () {
-    // Check if plain text wrapped in <pre> element exists and doesn't exceed maxBodyLength
-    const pre = document.body.querySelector('pre')
-    const rawBody = pre && pre.innerText
+  .metric      { color: #000 }
+  .value       { color: #ff20ed }
+  .label-key   { color: blue }
+  .label-value { color: green }
+  `)
 
-    if (!rawBody || rawBody.length > maxBodyLength) {
-      port.disconnect()
-      return
-    }
+const port = chrome.runtime.connect({ name: 'promformat' })
 
-    // Post the contents of the PRE
-    port.postMessage({
-      name: 'SENDING TEXT',
-      payload: rawBody
-    })
+// Add listener to receive response from BG when ready
+port.onMessage.addListener(msg => {
+  switch (msg.name) {
+    case 'FORMATTED' :
+      // Insert CSS
+      const promformatStyle = document.createElement('style')
+      document.head.appendChild(promformatStyle)
+      promformatStyle.insertAdjacentHTML('beforeend', style)
+
+      // Insert HTML content
+      const promformatContent = document.createElement('div')
+      promformatContent.id = 'promformat'
+      document.body.appendChild(promformatContent)
+
+      promformatContent.innerHTML = msg.payload
+      break
+
+    default :
+      throw new Error(`Message not understood: ${msg.name}`)
+  }
+})
+
+function ready (data) {
+  // Check if it is a Prometheus plain text response
+  // This is quite a basic assumption, as the browser cannot access the
+  // 'version' part of the content type to verify.
+  const paths = data.paths.length ? data.paths : []
+
+  if (document.contentType !== 'text/plain') {
+    return
   }
 
-  document.addEventListener('DOMContentLoaded', () => {
-    chrome.storage.sync.get({ paths: [] }, ready)
+  for (var i = 0; i < paths.length; ++i) {
+    if (document.location.pathname.match(paths[i])) {
+      format()
+      break
+    }
+  }
+}
+
+function format () {
+  // Check if plain text wrapped in <pre> element exists and doesn't exceed maxBodyLength
+  const pre = document.body.querySelector('pre')
+  const rawBody = pre && pre.innerText
+
+  if (!rawBody || rawBody.length > maxBodyLength) {
+    port.disconnect()
+    return
+  }
+
+  // Post the contents of the PRE
+  port.postMessage({
+    name: 'SENDING TEXT',
+    payload: rawBody
   })
-})()
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  chrome.storage.sync.get({ paths: [] }, ready)
+})
