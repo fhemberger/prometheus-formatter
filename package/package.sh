652ac36ff644c5abc76ef1afaa9abf660d9405b9
Frederic Hemberger <mail@frederic-hemberger.de>
2023-02-25 16:50:21 +0100

    Fix packaging script, remove macOS specific files

--- package/package.sh
+++ package/package.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2155
 set -uo pipefail
 
 readonly DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 function package_zip () {
-  cd "$1" && zip -r "$2" html icons js LICENSE.txt manifest.json -x icons/.DS_Store js/.DS_Store .DS_Store
+  (
+  cd "$1" || exit
+  zip -rX "$2" html icons js LICENSE.txt manifest.json -x '**/.*' -x '**/__MACOSX'
+  )
 }
 
 # Update manifest version number from package.json
@@ -14,7 +18,7 @@ function package_zip () {
 package_zip "$DIR/../extension" "$DIR/../chrome-webstore-release.zip"
 
 # Package Firefox webstore release
-cp -r "$DIR/../extension" "$DIR/../extension-firefox"
+cp -Tr "$DIR/../extension" "$DIR/../extension-firefox"
 "$DIR/update-manifest-firefox.js"
 package_zip "$DIR/../extension-firefox" "$DIR/../firefox-webstore-release.zip"
 #rm -r "$DIR/../extension-firefox"
